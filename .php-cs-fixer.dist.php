925530954fd9222075574e25733c5321eb7fdc08
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2023-07-05 18:50:51 +0300

    refactor: Merge upstream

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,10 +5,11 @@ require __DIR__ . '/vendor/tenantcloud/php-cs-fixer-rule-sets/bootstrap.php';
 
 use PhpCsFixer\Finder;
 use TenantCloud\PhpCsFixer\Config;
-use TenantCloud\PhpCsFixer\RuleSet\TenantCloudSet;
 
 $finder = Finder::create()
 	->in(__DIR__)
+	->exclude('build')
+	->exclude('tmp')
 	->exclude('vendor')
 	->name('*.php')
 	->notName('_*.php')
